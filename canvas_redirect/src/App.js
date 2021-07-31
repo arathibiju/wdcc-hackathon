2823b4e4866140a7998e18eb32ce5ed6fb8528f6
Julia Shan <juliashan111@gmail.com>
2021-07-31 18:43:39 +1200

    Update UI

--- canvas_redirect/src/App.js
+++ canvas_redirect/src/App.js
@@ -3,25 +3,28 @@ import postsList from './post.js';
 
 
 function App() {
-
- 
-
   return (
-
       <div className="App">
+
         <header className="App-header">
           <p>Sorry, Canvas can't be accessed right now</p>  
         </header>
-        <div className="flex-row">
-              <div className="flex-column">
-                reddit
-                <div>
-                  {postsList()}
-                </div>
+
+        <div className="data">
+              <div className="posts">
+                <h1>Reddit</h1>
+                <div>{postsList()}</div>
               </div>
                 
-              <div className="flex-column">instagram</div>
-              <div className="flex-column">twitter</div>
+              <div className="posts">
+                <h1>Instagram</h1>
+              <div>{postsList()}</div>
+              </div>
+
+              <div className="posts">
+                <h1>Twitter</h1>
+              <div>{postsList()}</div>
+              </div>
         </div>
         
 
