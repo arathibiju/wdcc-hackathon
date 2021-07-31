4c2ad19d2eea1149adbbc43a54906a9fd0c4caae
karenshew <kshe267@aucklanduni.ac.nz>
2021-07-31 19:54:37 +1200

    Added icons for extension

--- chrome extension/background.js
+++ chrome extension/background.js
@@ -1,4 +1,4 @@
-// Listens for a message from contentScript.js and updates the url of the tab to redirect to provided url
+// Listens for a message from contentScript.js and updates the url of the tab to redirect to given url
 chrome.runtime.onMessage.addListener(function(request, sender) {
     chrome.tabs.update(sender.tab.id, {url: request.redirect});
 })
