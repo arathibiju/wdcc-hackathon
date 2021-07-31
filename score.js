1b2e2788886a2ddb071e3046f7bf7b92cc4dedf3
Manoj <58104995+BlackLopaz@users.noreply.github.com>
2021-07-31 17:02:58 +1200

    Setup timer to update score every second

--- score.js
+++ score.js
@@ -4,23 +4,55 @@ class Score extends React.Component {
 
     constructor(props) {
         super(props);
+        this.minProcrastinateTime = 30000; //30000 milliseconds = 30 seconds 
+        this.timeDifference = 0;
+        this.startTime = Date();
+        this.overallScore = 0; //Should read from file - from extension?
         //Only have start and end time based on while the user is on the page - therefore if they navigate off their score will be reset
-        this.state = {startTime: Date(), endTime: new Date(), score: 0 };
+        this.state = {endTime: Date(), currentScore: 0 };
         document.addEventListener("visibilitychange", function() {
-            if(document.visibilityState == "visible") {
-                this.state.score = 0;
+            if(document.visibilityState == "hidden") {
+                //if()
+                this.setState({
+                    currentScore: 0,
+                    endTime: this.startTime,
+                });
             }
+            else if(document.visibilityState == "visible") {
+                this.startTime = Date();
+            }
+
         });
     }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.calculateTime(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
     //React context to create singleton or use local storage - better to use singleton
+    
+    calculateTime() {
+        this.setState({
+            endTime: Date(),
+            currentScore: endTime - this.startTime,
+        });
+    }
 
     render() {
         return (
             <div>
-                Your current score is: {this.state.score}
+                Your current score is: {this.state.currentScore}
             </div>
         )
     }
+
+    
     //Time tracking
 
     //Score calculation
