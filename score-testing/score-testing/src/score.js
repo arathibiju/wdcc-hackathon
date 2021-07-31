31ecb5ca467552fa88c9a01eb50d0568b78fff37
Manoj <58104995+BlackLopaz@users.noreply.github.com>
2021-07-31 18:15:04 +1200

    Fixed context issue

--- score-testing/score-testing/src/score.js
+++ score-testing/score-testing/src/score.js
@@ -10,7 +10,7 @@ class Score extends React.Component {
         this.overallScore = 0; //Should read from file - from extension?
         //Only have start and end time based on while the user is on the page - therefore if they navigate off their score will be reset
         this.state = {endTime: Date.now(), currentScore: 0 };
-        document.addEventListener("visibilitychange", function() {
+        document.addEventListener("visibilitychange", () => {
             if(document.visibilityState == "hidden") {
                 if(this.state.currentScore >= 30) {
                     this.overallScore += this.state.currentScore;
@@ -48,7 +48,7 @@ class Score extends React.Component {
         console.log("Calculating new score");
         this.setState({
             endTime: Date.now(),
-            currentScore: (this.state.endTime - this.startTime) * 1000,
+            currentScore: Math.trunc((this.state.endTime - this.startTime) / 1000),
         });
     }
 
@@ -59,13 +59,6 @@ class Score extends React.Component {
             </div>
         )
     }
-
-    
-    //Time tracking
-
-    //Score calculation
-
-    //Return data
 
 }
 
