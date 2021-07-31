d3845e7370120b6f7e6692e5fc130b91c86fdb4d
Manoj <58104995+BlackLopaz@users.noreply.github.com>
2021-07-31 16:21:30 +1200

    Added event listener to reset score on tab switch

--- score.js
+++ score.js
@@ -6,6 +6,11 @@ class Score extends React.Component {
         super(props);
         //Only have start and end time based on while the user is on the page - therefore if they navigate off their score will be reset
         this.state = {startTime: Date(), endTime: new Date(), score: 0 };
+        document.addEventListener("visibilitychange", function() {
+            if(document.visibilityState == "visible") {
+                this.state.score = 0;
+            }
+        });
     }
     //React context to create singleton or use local storage - better to use singleton
 
