28b3866cbb09e5ddd42da4f827899aa0093fb996
Arathi Biju <abij996@aucklanduni.ac.nz>
2021-08-01 11:50:37 +1200

    added api for getting twitter posts

---
+++ canvas_redirect/src/TwitterPosts.js
@@ -0,0 +1,30 @@
+import { React,  useState, useEffect } from "react";
+//import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+//import InstagramEmbed from 'react-instagram-embed';
+
+
+const TwitterContainer = () => {
+    useEffect(() => {
+      const anchor = document.createElement("a");
+      anchor.setAttribute("class", "twitter-timeline");
+      anchor.setAttribute("data-theme", "dark");
+      anchor.setAttribute("data-tweet-limit", "10");
+      anchor.setAttribute("data-chrome", "noheader nofooter noborders");
+      anchor.setAttribute("href", "https://twitter.com/HeyMarkKop");
+      document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
+  
+      const script = document.createElement("script");
+      script.setAttribute("src", "https://platform.twitter.com/widgets.js");
+      document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+    }, []);
+  
+    return (
+      <section className="twitterContainer">
+        <div className="twitter-embed"></div>
+      </section>
+    );
+  };
+
+export default TwitterContainer;
+ 
+
