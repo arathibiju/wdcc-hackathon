064c4b402b144e39273bc144e8739708575b5f88
Manoj <58104995+BlackLopaz@users.noreply.github.com>
2021-08-01 13:50:53 +1200

    Completed methods for making new user and reading data from database

--- leaderboard-database/app-server.js
+++ leaderboard-database/app-server.js
@@ -16,8 +16,11 @@ client.connect(function(err) {
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
-  insertDocuments(db, function() {
+  /*insertDocuments(db, function() {
     client.close();
+  });*/
+  findDocuments(db, function() {
+      client.close();
   });
   //client.close();
 });
@@ -37,6 +40,23 @@ const insertDocuments = function(db, callback) {
       callback(result);
     });
 }
+
+const findDocuments = function(db, callback, userId) {
+    // Get the documents collection
+    const collection = db.collection('leaderboard');
+    // Find some documents
+    collection.find({'userId': userId}).toArray(function(err, docs) {
+        userData = docs[0];
+      assert.equal(err, null);
+      //console.log(docs[0]);
+      //var userData = toObject(docs[0]);
+      //var userData = JSON.parse(docs[0]);
+      console.log("Found the following records");
+      //console.log(docs[0].Id);
+      console.log(`User Id: ${userData.Id} \nSession Score: ${docs[0].SessionScore} \nOverall Score: ${docs[0].OverallScore}` );
+      callback(docs);
+    });
+  }
 
 class UserData {
     constructor(userId, userSessionScore, userOverallScore) {
